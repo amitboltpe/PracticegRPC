9d31985be9244aec7153fd2848baefc1c7fd62d6
Amit Jain <amitjain@Mac.lan>
2025-11-07 11:48:36 +0530

    add code of pdf compress

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module testpro
 
 go 1.25.3
+
+require github.com/pdfcpu/pdfcpu v0.11.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("hellos")
+
+	CompressPdf("./30MBFILE.pdf", "OutputFilePath.pdf")
+}
+
+func CompressPdf(inputFilePath, OutputFilePath string) {
+
+	input := inputFilePath
+	output := OutputFilePath
+	cmd := exec.Command("gs",
+		"-sDEVICE=pdfwrite",
+		"-dCompatibilityLevel=1.4",
+		"-dPDFSETTINGS=/screen", // /screen, /ebook, /printer, /prepress
+		"-dNOPAUSE",
+		"-dQUIET",
+		"-dBATCH",
+		"-sOutputFile="+output,
+		input,
+	)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Ghostscript compression failed: %v", err)
+	}
+	fmt.Println("PDF compressed:", output)
 }
